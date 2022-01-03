b0efd82120623b28a310e48ea92a86501f92d382
tkskto <tkskto@gmail.com>
2022-01-03 17:44:32 +0900

    画像のパス変更

--- js/gif.js
+++ js/gif.js
@@ -2,29 +2,36 @@
     'use strict'; // おまじない
 
     const setScrollGif = () => {
-        // idが"img"の要素をcornという名前の箱に入れておく
+        // classが"item"の要素をitemListという名前の箱に入れておく
         const itemList = document.querySelectorAll('.item');
 
         itemList.forEach((item) => {
             let index = 0;
+            // itemListが持っているimgタグを取得
             let image = item.querySelectorAll('img');
 
+            // imageから1個ずつ取り出して処理する
             image.forEach((img) => {
+                // 取り出したimgタグにクラスをつける
                 img.classList.add('-hide');
             });
-            image.item(index).classList.remove('-hide');
+            // 1個目だけ表示
+            image.item(0).classList.remove('-hide');
 
-            document.addEventListener('scroll', () => {
-                image.item(index).classList.add('-hide');
+            document.addEventListener('scroll',
+                () => {
+                    image.item(index).classList.add('-hide');
 
-                index++; // 番号を+1する
+                    index = index + 1; // 番号を+1する
 
-                if (index > image.length - 1) {
-                    index = 0;
+                    // 最後の画像を表示したら0番目に戻る
+                    if (index > image.length - 1) {
+                        index = 0;
+                    }
+
+                    image.item(index).classList.remove('-hide');
                 }
-
-                image.item(index).classList.remove('-hide');
-            });
+            );
         });
     };
     setScrollGif();
